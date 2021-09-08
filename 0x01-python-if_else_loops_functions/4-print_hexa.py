4765f7232506cc942963245fe22b8ec6e3536aff
Andres Blandon <andblandon@hotmail.com>
2021-09-08 04:17:19 +0000

    task 4 update

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(0,99):
-    if i < 99:
-        print("{:02d}".format(i), end=', ')
-    else:
-        print(i)
+for i in range(0, 99):
+    print("{} = 0x{}".format(i, '%0x' % i))
