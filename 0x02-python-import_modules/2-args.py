656204a5428186a13262d0ba5726424fe4dd026c
Andres Blandon <andblandon@hotmail.com>
2021-09-09 03:14:10 +0000

    synamic script update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ lon = len(sys.argv)
 if lon != 0:
     print("{} arguments:".format(lon-1))
     for i in range(1, lon):
-        print(i, ":", str(sys.argv[i]))
+        print("{}:".format(i), str(sys.argv[i]))
 else:
     print("0 arguments.")
