872acdb0a39955a33c30c5f131c65dd96f694a83
Andres Blandon <andblandon@hotmail.com>
2021-09-09 03:11:17 +0000

    synamic script tk2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+lon = len(sys.argv)
+if lon != 0:
+    print("{} arguments:".format(lon-1))
+    for i in range(1, lon):
+        print(i, ":", str(sys.argv[i]))
+else:
+    print("0 arguments.")
