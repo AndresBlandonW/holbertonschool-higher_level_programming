51c8f86f0f41d83ab816828184a99f57789cd0b8
Andres Blandon <andblandon@hotmail.com>
2021-09-13 01:59:24 +0000

    task 6 - python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,25 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    lon = len(sys.argv)
+    lon = len(sys.argv) - 1
 
     if (lon != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[3])
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     ope = str(sys.argv[2])
 
     if ope == "+":
-        print("{} {} {} = {}".format(num1, num2, ope, add(num1, num2)))
+        print("{} {} {} = {}".format(a, ope, b, add(a, b)))
     elif ope == "-":
-        print("{} {} {} = {}".format(num1, num2, ope, sub(num1, num2)))
+        print("{} {} {} = {}".format(a, ope, b, sub(a, b)))
     elif ope == "/":
-        print("{} {} {} = {}".format(num1, num2, ope, div(num1, num2)))
+        print("{} {} {} = {}".format(a, ope, b, div(a, b)))
     elif ope == "*":
-        print("{} {} {} = {}".format(num1, num2, ope, mul(num1, num2)))
+        print("{} {} {} = {}".format(a, ope, b, mul(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    exit(0)
