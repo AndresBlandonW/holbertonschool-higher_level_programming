87ef3ee3ecddcf2fd07082b0184d71e1f7f56186
Andres Blandon <andblandon@hotmail.com>
2021-09-14 02:48:11 +0000

    add reque

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    pos = my_list[idx]
-    my_list.remove(pos)
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        pos = my_list[idx]
+        my_list.remove(pos)
