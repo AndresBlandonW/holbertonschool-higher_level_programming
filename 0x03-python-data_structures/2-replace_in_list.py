971206901454784a5712ce1794ba6db0cfc97002
Andres Blandon <andblandon@hotmail.com>
2021-09-14 02:23:23 +0000

    resolve error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    cop_my_list = my_list.copy()
     if idx >= len(my_list):
-        return cop_my_list
+        return my_list
     elif idx < 0:
-        return cop_my_list
+        return my_list
     else:
-        cop_my_list[idx] = element
-        return cop_my_list
+        my_list[idx] = element
+        return my_list
