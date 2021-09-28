a2936431b053973d12ebdd61aa8c653c3d828556
Andres Blandon <andblandon@hotmail.com>
2021-09-28 04:43:49 +0000

    task 9 update stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
+
     try:
         print("{:d}".format(value))
         return True
-    except Exception as ne:
-        print("Exception: {}".format(ne))
+    except (ValueError, TypeError) as ne:
+        sys.stderr.write("Exception: {}".format(ne))
         return False
