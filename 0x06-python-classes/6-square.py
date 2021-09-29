8f98473f23eb19866852d7dbd960db28e4344277
Andres Blandon <andblandon@hotmail.com>
2021-09-29 05:06:27 +0000

    tk6 coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,11 +17,13 @@ class Square:
     @size.setter
     def size(self, value):
         """setter the size"""
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
     @property
     def position(self):
@@ -31,11 +33,12 @@ class Square:
     @position.setter
     def position(self, value):
         """Setter the position"""
-        if type(value) is not tuple or len(value) is not 2 or \
-                type(value[0]) is not int or type(value[1]) \
-                is not int or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """Return the area of square"""
