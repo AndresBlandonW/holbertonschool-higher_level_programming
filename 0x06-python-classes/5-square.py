fe63c4bffa1b700e686959fe421a58c7e5685b77
Andres Blandon <andblandon@hotmail.com>
2021-09-29 04:18:42 +0000

    tk5 print char

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Create a Square class"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initialize data, and check size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Return size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter the size"""
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Return the area of square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print with character (#)"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
