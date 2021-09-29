5f63b733a0459f4cc2511148c7ec548718a01efd
Andres Blandon <andblandon@hotmail.com>
2021-09-29 04:02:13 +0000

    task 1 define

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a Square class"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Initialize a size"""
+        self.__size = size
