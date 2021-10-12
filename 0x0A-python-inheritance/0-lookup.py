c07dec2c008dc76696183d773ceb30eb5329f3ba
Andres Blandon <andblandon@hotmail.com>
2021-10-12 13:04:50 +0000

    tk0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """
+    Check available attributes and methods of an object
+
+    Return - list data
+    """
+    return dir(obj)
