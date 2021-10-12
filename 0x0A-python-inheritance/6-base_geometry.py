90596f9d94cb01c77509e174ca5d07dfe1d7bfcc
Andres Blandon <andblandon@hotmail.com>
2021-10-12 04:47:47 +0000

    tk6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Write an empty class"""
+    pass
+
+    def area(self):
+        """
+        Not implemented
+        """
+        raise Exception('area() is not implemented')
