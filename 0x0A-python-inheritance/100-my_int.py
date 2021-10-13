787b180d50ad6aa9106b0ebb6fe2dd84db9342c6
Andres Blandon <andblandon@hotmail.com>
2021-10-13 22:28:24 +0000

    tk12 upd

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,8 +5,10 @@
 class MyInt(int):
     def __eq__(self, value):
         """Redefine equality to be unequal"""
-        return self.value == value.value
+        if isinstance(type(value), type(self)):
+            return self == value
+        return False
 
     def __ne__(self, value):
-        """Delega el resultado"""
-        return not self == value
+        """Delegate the result"""
+        return not self.__eq__(value)
