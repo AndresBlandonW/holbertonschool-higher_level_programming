db88cbbff52b8c501802ba8a7e7f0fee02f69887
Andres Blandon <andblandon@hotmail.com>
2021-10-13 04:46:32 +0000

    tk11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Defines a student by first name, last name, age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if type(attrs) is list and all(isinstance(i, str) for i in attrs):
+            new_dict = {}
+            for k in list(self.__dict__.keys()):
+                if k in attrs:
+                    new_dict[k] = self.__dict__[k]
+            return new_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        if 'first_name' in json:
+            self.first_name = json['first_name']
+        if 'last_name' in json:
+            self.last_name = json['last_name']
+        if 'age' in json:
+            self.age = json['age']
