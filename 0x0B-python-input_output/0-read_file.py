f9b54bdc78e402de6933df78b1c9445721dae7ea
Andres Blandon <andblandon@hotmail.com>
2021-10-13 03:08:38 +0000

    tk0 upd

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """function that reads a text file"""
 
+
 def read_file(filename=""):
-    with open(filename) as f:
+    """
+    read content file
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
         read_data = f.read()
     for line in read_data:
         print(line, end='')
