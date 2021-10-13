807abefcc52ab74f2d719504b96122a84e14c750
Andres Blandon <andblandon@hotmail.com>
2021-10-13 22:11:03 +0000

    tk11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
         """
         Init method for Square
         """
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
