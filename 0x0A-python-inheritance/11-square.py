9f9d954f09533c9d693851d2212b7a541d5379ef
Andres Blandon <andblandon@hotmail.com>
2021-10-13 02:40:43 +0000

    upd error

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,13 +7,17 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Class Square that inherits from Rectangle"""
-    def __init__(self, size=0):
+    def __init__(self, size):
         """
         Init method for Square
         """
-        self.__size = size
         super().integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return area of REctangle object"""
+        return self.__size * self.__size
 
     def __str__(self):
         """Print description of Square"""
