03ac1fdeb231dedb9ce12bffa4b34a5f1a80aca5
Andres Blandon <andblandon@hotmail.com>
2021-10-23 02:52:38 +0000

    base update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,10 +10,10 @@ class Base:
     def __init__(self, id=None):
         """Init method for Base class"""
         if id is not None:
-            self.id = int(id)
+            self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
