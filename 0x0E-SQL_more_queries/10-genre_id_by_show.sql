5d52fbebfc41735ee786feaa33cb3877b66c5cf3
AndresBlandonW <andblandon@hotmail.com>
2021-11-17 15:04:37 -0800

    Script Genre ID by show Mysql update

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,6 @@
--- List all shows contained in DB that have at least one genre linked
-SELECT tv.title, g.genre_id FROM tv_shows as tv INNER JOIN tv_show_genres as g ON tv.id = g.show_id ORDER BY tv.title, g.genre_id;
+-- List all shows contained in DB that have at least
+SELECT tv.title, g.genre_id
+       FROM tv_shows as tv
+       INNER JOIN tv_show_genres as g
+       ON tv.id = g.show_id
+       ORDER BY tv.title, g.genre_id;
