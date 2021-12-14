d103ef58b0db20547b788d0acfee338ee1290969
Andres Blandon <andblandon@hotmail.com>
2021-12-14 04:58:31 +0000

    second buggest argument js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv;
-arg.splice(0, 2);
-
-if (arg.length > 3) {
-  arg.sort(function (a, b) { return b - a; });
-  console.log(arg[1]);
-} else {
-  console.log(0);
+const arg = process.argv.slice(2);
+let big = 0;
+if (arg.length > 1) {
+  arg.sort((a, b) => a - b);
+  big = arg[arg.length - 2];
 }
+console.log(big);
