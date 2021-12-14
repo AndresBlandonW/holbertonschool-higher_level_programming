dc180f4ef25a92f6d779b3a07bcc9c409cfc61e1
Andres Blandon <andblandon@hotmail.com>
2021-12-14 04:16:39 +0000

    multiple prints js update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-const c = 'C is fun';
-const py = 'Python is cool';
-const js = 'Javascript is amazing';
-
-console.log('%s\n%s\n%s', c, py, js);
+const fl = 'C is fun';
+const sl = 'Python is cool';
+const tl = 'JavaScript is amazing';
+console.log('%s\n%s\n%s', fl, sl, tl);
