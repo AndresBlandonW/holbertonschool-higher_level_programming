0f21192f77392e5542f19eaa0e3eca607b9a1284
Andres Blandon <andblandon@hotmail.com>
2021-12-14 04:31:34 +0000

    adds values

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
-  if (!isNaN(process.argv[3]) && process.argv[3] && !isNaN(process.argv[2]) && process.argv[2]) {
-    console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
-  }
+    console.log(a + b);
 }
-
-add();
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+add(a, b);
