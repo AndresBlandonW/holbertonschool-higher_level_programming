67a72c970d1b8fa2872f1ee3ae7c2d6ccfa638f8
Andres Blandon <andblandon@hotmail.com>
2021-12-14 04:30:03 +0000

    script Update error

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length < 2) {
+if (process.argv.length < 3) {
   console.log('undefined'.concat(' is ').concat('undefined'));
 } else if (process.argv.length === 3) {
   console.log(process.argv[2].concat(' is ').concat('undefined'));
