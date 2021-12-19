04dbe41fb7b9bb2869a80166915a249a2dc502ec
Andres Blandon <andblandon@hotmail.com>
2021-12-19 23:25:17 +0000

    lists all states sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
-from sys import argv
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
