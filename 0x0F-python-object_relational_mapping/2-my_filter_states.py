b7d6d93c0a6b5121ba6bbe32d236175c1cfcd440
Andres Blandon <andblandon@hotmail.com>
2021-12-20 01:08:32 +0000

    display all values args matches tk2 sql

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}'".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
