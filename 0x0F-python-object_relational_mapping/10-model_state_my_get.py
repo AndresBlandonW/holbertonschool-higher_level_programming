c7e8893433628e1746ecaaa119e828c7ada79a46
Andres Blandon <andblandon@hotmail.com>
2021-12-20 02:00:50 +0000

    Update red tk10 sql

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""prints the State object with the name passed as argument from the database hbtn_0e_6_usa"""
+"""prints the State object with the name passed as argument from the db"""
 
 if __name__ == "__main__":
     import sys
