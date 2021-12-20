3c8ed23adb31a140b4bfa7e1799f6db6fe2a6032
Andres Blandon <andblandon@hotmail.com>
2021-12-20 01:15:30 +0000

    Update tk1 sql - main error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import MySQLdb
-from sys import argv
+"""lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
