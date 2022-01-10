887d3abd5f9853e4ccc50acb0e92c1593697113b
AndresBlandonW <andblandon@hotmail.com>
2022-01-09 19:21:38 -0800

    status fetches

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches url https://intranet.hbtn.io/status"""
+from urllib.request import urlopen, Request
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    req = Request(url)
+    with urlopen(url) as response:
+        read = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(read)))
+        print('\t- content: {}'.format(read))
+        print('\t- utf8 content: {}'.format(read.decode("UTF-8")))
