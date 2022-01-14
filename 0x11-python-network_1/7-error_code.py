9c45a9e4e21c1bfd6bf47d50f7740b154eedc03f
Andres Blandon <andblandon@hotmail.com>
2022-01-14 02:58:58 +0000

    error code 1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    r = get(argv[1])
+    if r.status_code >= 400:
+            print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.text)
