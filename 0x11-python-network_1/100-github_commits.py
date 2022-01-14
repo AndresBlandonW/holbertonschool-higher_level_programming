df5117f0bda2cea7910d5dcad6204d3965250012
Andres Blandon <andblandon@hotmail.com>
2022-01-14 05:00:16 +0000

    tk100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List 10 commits (from the most recent to oldest) of the  by the 
+"""
+from requests import get, post
+from sys import argv
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = get(url)
+    commits = r.json()
+    counter = 0
+    for commit in commits:
+        if counter == 10:
+            break
+        print('{}: {}'.format(commit.get('sha'),
+                              commit.get('commit').get('author').get('name')))
+        counter += 1railsuser railsrepository
+   
