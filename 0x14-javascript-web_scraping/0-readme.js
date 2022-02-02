b04563871cdf5b248feab58b60000627c672be1d
Andres Blandon <andblandon@hotmail.com>
2022-02-02 00:54:07 +0000

    reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const file = process.arg[2];
-fs.eadFile(file, 'utf-8', function (err, data) {
+fs.readFile(file, 'utf-8', function (err, data) {
     if (err) {
 	console.log(err);
     } else {
