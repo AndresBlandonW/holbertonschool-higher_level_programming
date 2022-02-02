77221e177396f8c161f6eb80339f7b7ca5396426
Andres Blandon <andblandon@hotmail.com>
2022-02-02 00:57:25 +0000

    Update req

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.arg[2];
+const file = process.argv[2];
 fs.readFile(file, 'utf-8', function (err, data) {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
