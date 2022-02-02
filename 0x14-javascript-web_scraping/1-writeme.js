f49ec0aa7cc4b3c32f139339f0bdf8c191fe0b42
Andres Blandon <andblandon@hotmail.com>
2022-02-02 01:01:55 +0000

    write str to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const cont = process.argv[3];
+fs.writeFile(file, cont, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
