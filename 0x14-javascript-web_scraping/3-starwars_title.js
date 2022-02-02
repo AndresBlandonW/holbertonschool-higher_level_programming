bf20336749220dd172873debd72911900541fff3
Andres Blandon <andblandon@hotmail.com>
2022-02-02 01:45:16 +0000

    request to JSON URL v2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
-request (url, function(error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
